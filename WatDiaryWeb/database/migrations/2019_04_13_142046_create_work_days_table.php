8e6d915f17e64d391c3ea91dbae8348062c1098d
Mehmet CANHOROZ <mehmetcanhoroz@gmail.com>
2019-04-13 18:21:46 +0300

    First commit and created a few model and migration

---
+++ WatDiaryWeb/database/migrations/2019_04_13_142046_create_work_days_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_days', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_days');
+    }
+}
